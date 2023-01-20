805a0fe97f20f185a6879e997642b27822b84cfe
choilettuce <tkdgnstkdcn@naver.com>
2023-01-20 16:16:04 +0900

    폴더정리

---
+++ TS-intermediate/02_transfer.js
@@ -0,0 +1,14 @@
+// 서버에서 다른 서버로 정보 보내주기때 활용
+// 프론트에서 서버로부터 받을 때 -> jason object
+var LoginDatatransfer = /** @class */ (function () {
+    function LoginDatatransfer(ID, PW) {
+        if (PW === void 0) { PW = "default password"; }
+        this.ID = ID;
+        if (PW)
+            this._PW = PW;
+    }
+    return LoginDatatransfer;
+}());
+var LoginDTO = new LoginDatatransfer("hi", "1234");
+// LoginDTO.ID = "set";
+console.log(LoginDTO);
---
+++ TS-intermediate/03_uniiner.ts
@@ -0,0 +1,35 @@
+//1. union - 합집합 |(or)
+let nameOrAge : string | number;
+nameOrAge = "lettuce"
+nameOrAge = 31;
+
+interface IName {
+    name : string;
+}
+interface IAge {
+    age : number;
+}
+
+function prtUnion(params : any) {
+    // name 정보가 들어왔을때 로직
+    if ("name" in params) {
+        console.log(params.name);
+    }
+    // age 정보가 들어왔을때 로직
+    if ("age" in params) {
+        console.log(params.age);
+    }
+}
+
+prtUnion({name : "lettuce"})
+//2. intersection - 교집합 &(And)
+// interface IName {
+//     name : string;
+// }
+// interface IAge {
+//     age : number;
+// }
+// const nameAndAge : IName & IAge = {
+//     name : "lettuce"
+
+// }
