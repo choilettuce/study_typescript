0418d17de95fa7fb0c0e820479571b0b5f62059e
choilettuce <tkdgnstkdcn@naver.com>
2023-01-20 16:25:45 +0900

    폴더정리

--- TS-intermediate/01_class.js
+++ TS-intermediate/01_class.js
@@ -1,18 +1,25 @@
 // 접근 제한자 public protected private
-class UserInfo {
-    constructor(name, age, city) {
+var UserInfo = /** @class */ (function () {
+    function UserInfo(name, age, city) {
         this.name = name;
         this.age = age;
         this._city = city;
     }
-    get city() {
-        return this._city;
-    }
-    set city(newcity) {
-        this._city = newcity;
-    }
-}
-const user = new UserInfo("용수", 25, "seoul");
+    Object.defineProperty(UserInfo.prototype, "city", {
+        get: function () {
+            return this._city;
+        },
+        // 세터는 속력, 넓이, 나이 등은 음수를 쓸 수 없는데
+        // 
+        set: function (newcity) {
+            this._city = newcity;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return UserInfo;
+}());
+var user = new UserInfo("용수", 25, "seoul");
 // user.age = 27;
 // console.log(user.name, user.age, user.city)
 user.city = "incheon";
